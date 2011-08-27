5914222aec82351244cc8d5e0d079592589245e2
MURAOKA Taro <koron.kaoriya@gmail.com>
2011-08-28 00:11:30 +0900

    finish for less5 pattern

--- SoloGame/solve.pl
+++ SoloGame/solve.pl
@@ -4,6 +4,8 @@ use utf8;
 use strict;
 use warnings;
 
+my $DEBUG = 1;
+
 sub solve_all {
     my ($IN) = @_;
 
@@ -14,8 +16,11 @@ sub solve_all {
         chomp $line;
         my @n = split(/ /, $line);
         my ($answer, $state) = &solve_one(\@n);
-        #printf "%d\n", $answer;
-        printf "%d :%s\n", $answer, $state;
+        if ($DEBUG) {
+            printf "%d :%s\n", $answer, $state;
+        } else {
+            printf "%d\n", $answer;
+        }
     }
 }
 
@@ -47,11 +52,16 @@ sub solve_one {
         &pad_elem($elem, $max);
     }
 
+    # Calc sum of "AND".
+    my $andsum = &and_all(\@elements);
+
     # Show status.
     foreach my $elem (@elements) {
-        printf "  %8d: %s\n", $elem->{value}, $elem->{padmap};
+        printf("  %8d: %s (%08X)\n", $elem->{value}, $elem->{padmap},
+            $elem->{padval});
     }
-    printf "  max=%d has5=%d no5min=%d\n", $max, $has5, $no5min;
+    printf("  cnt=%d max=%d has5=%d no5min=%d andsum=%08X\n",
+        scalar(@elements), $max, $has5, $no5min, $andsum);
 
     if ($no5min == $max) {
         return ($no5min, 'NO5MIN');
@@ -73,10 +83,62 @@ sub solve_one {
     }
 
     if ($has5 < scalar(@elements)) {
-        return (0, 'LESS5');
+        return &solve_less5(\@elements);
     } else {
-        return (0, 'FULL5');
+        return &solve_full5(\@elements);
     }
+}
+
+sub solve_less5 {
+    my ($array) = @_;
+    my ($array1, $array2) = ([], []);
+
+    my $max_no5 = 0;
+    foreach my $item (@$array) {
+        if ($item->{has5}) {
+            push @$array2, $item;
+        } else {
+            push @$array1, $item;
+            if ($item->{len} > $max_no5) {
+                $max_no5 = $item->{len};
+            }
+        }
+    }
+
+    my ($answer, $status) = &solve_full5($array2);
+
+    if ($answer > $max_no5) {
+        return ($answer, 'LESS5_'.$status);
+    } else {
+        return ($max_no5, 'LESS5');
+    }
+}
+
+sub solve_full5 {
+    my ($array) = @_;
+
+    my $maxlen = &max_len($array);
+
+    # Found solid answer.
+    my $andsum = &and_all($array);
+    printf "  andsum=%08X\n", $andsum;
+    my $min1 = 0;
+    for (my $i = 1; $andsum > 0 and $i <= 32; ++$i) {
+        if (($andsum & 1) != 0) {
+            $min1 = $i;
+            last;
+        }
+        $andsum >>= 1;
+    }
+
+    # FIXME: Not perfect.  But enough for current inputs.
+    if ($min1 > 0 and $min1 < $maxlen) {
+        return ($min1, 'FULL5_MIN1');
+    }
+
+    # TODO:
+
+    return (999, 'FULL5_TODO');
 }
 
 sub make_elem {
@@ -88,6 +150,7 @@ sub make_elem {
         len=>length($rawmap),
         rawmap=>$rawmap,
         padmap=>undef,
+        padval=>undef,
         has5=>$has5,
     };
 }
@@ -101,8 +164,19 @@ sub pad_elem {
     } else {
         $padmap = $elem->{rawmap};
     }
-    $padmap =~ s/...../$& /g;
+
+    my $padval = 0;
+    foreach my $ch (reverse split //, $padmap) {
+        $padval <<= 1;
+        if ($ch ne '/') {
+            $padval += 1;
+        }
+    }
+
+    $padmap =~ s/..../$& /g;
     $elem->{padmap} = $padmap;
+    $elem->{padval} = $padval;
+
     return $elem;
 }
 
@@ -135,6 +209,30 @@ sub get_rank {
         }
     }
     return ($rank1, $rank2);
+}
+
+sub and_all {
+    my ($elems) = @_;
+    my $retval;
+    foreach my $elem (@$elems) {
+        if (defined $retval) {
+            $retval &= $elem->{padval};
+        } else {
+            $retval = $elem->{padval};
+        }
+    }
+    return $retval;
+}
+
+sub max_len {
+    my ($elems) = @_;
+    my $retval = 0;
+    foreach my $elem (@$elems) {
+        if ($elem->{len} > $retval) {
+            $retval = $elem->{len};
+        }
+    }
+    return $retval;
 }
 
 &solve_all(\*STDIN);
