18c87cabc18459eeed777fe71132d653ebc5f91a
MURAOKA Taro <koron.kaoriya@gmail.com>
2011-08-29 03:29:46 +0900

    improve count script

--- SlidePuzzle/count.pl
+++ SlidePuzzle/count.pl
@@ -12,8 +12,13 @@ my %limit = (
     D => 81778,
 );
 my @order = qw(L R U D);
+my $num = 0;
+my $total = 0;
+
 while (<>) {
     chomp;
+    ++$num if length > 0;
+    ++$total;
     for my $ch (split //) {
         $count{$ch} += 1;
     }
@@ -24,3 +29,6 @@ for (@order) {
     my $l = $limit{$_};
     printf "%s: %5d/%5d (%.2f%%)\n", $_, $c, $l, ($c * 100 / $l);
 }
+
+print "\n";
+printf "TOTAL: %4d/%4d (%.2f%%)\n", $num, $total, ($num * 100 / $total);
