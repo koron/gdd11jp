1a0aa3aaf5f5664c1b52c176feb010b5684b5cc7
MURAOKA Taro <taro.muraoka@muraoka-design.com>
2011-08-29 17:00:08 +0900

    add merge script

---
+++ SlidePuzzle/merge.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+use utf8;
+use strict;
+use warnings;
+
+open IN1, $ARGV[0] or die "$!\n";
+open IN2, $ARGV[1] or die "$!\n";
+while (my $line1 = <IN1>) {
+    my $line2 = <IN2>;
+    last unless defined $line2;
+    chomp $line1;
+    chomp $line2;
+    if ($line1 ne '') {
+        print $line1, "\n";
+    } else {
+        print $line2, "\n";
+    }
+}
+close IN2;
+close IN1;
